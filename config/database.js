460f7c42047a6550b8eca1cb32fc723e02202a71
erikvictorino <erikvictorino1@gmail.com>
2026-07-27 19:56:29 -0300

    adiciona porta .env no database

--- config/database.js
+++ config/database.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize(
     {
     //local onde esta o banco
     host: process.env.DB_HOST,
+    port: process.env.PORT,
     //tipo de banco que esta sendo usado
     dialect: 'mysql'
 });
