6c608d005554ff689bd0f98984809a1d6036afcd
erikvictorino <erikvictorino1@gmail.com>
2026-07-30 10:39:53 -0300

    faz ajustes para deploy

---
+++ app/scripts/syncDatabase.js
@@ -0,0 +1,22 @@
+//script para sincronizar banco de dados ou subir alterações
+//para rodar alguma sincronização ou alteração no banco de dados
+//basta rodar node scripts/syncDatabase.js no terminal
+import "../models/Agendamento.js"
+import "../models/Barbeiro.js"
+import "../models/Cliente.js"
+import "../models/Servicos.js"
+//banco a conexão com o banco
+import conn from '../../config/database.js'
+
+//sincronização do banco de dados
+async function syncDatabase() {
+    try {
+        await conn.sync()
+        console.log('banco sincronizado')
+        process.exit(0)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+syncDatabase()
--- config/database.js
+++ config/database.js
@@ -5,28 +5,35 @@ dotenv.config()
 //importando ORM do projeto
 import {Sequelize} from 'sequelize'
 
+const dbConfig = {
+    //local onde esta o banco
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    //tipo de banco que esta sendo usado
+    dialect: 'mysql',
+}
+
+//criptografia da conexão com o banco de dados
+const useSSL = process.env.DB_SSL === 'true'
+
+if(useSSL){
+    dbConfig.dialectOptions = {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+}
+}
+
+
 //criando a conexão com banco de dados
 //com parametros, nome do banco, usuario, senha.
 const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER,
     process.env.DB_PASS, 
-    {
-    //local onde esta o banco
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    //tipo de banco que esta sendo usado
-    dialect: 'mysql'
-});
+    dbConfig
+);
 
-try {
-    //testando a conexão
-     await sequelize.authenticate()
-    //mensagem de sucesso
-    console.log('banco conectado com sucesso')
-} catch (err) {
-    //mostrando erro de conexão
-    console.log(`não foi possivel conectar: ${err}`)
-}
 //exportando a conexão com o banco de dados
 export default sequelize
